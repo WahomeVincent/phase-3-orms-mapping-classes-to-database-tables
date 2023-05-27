99732252cbd7c40304d76e3baa4d8b7e17a66a59
Wahome <vinwahome@gmail.com>
2023-05-27 09:16:29 +0300

    Completed Assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,39 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
   end
 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+
+  end
+
 end
+
+hello = Song.new(name: "Hello", album: "25")
+hello.save
+
+ninety_nine_problems = Song.new(name: "99 Problems", album: "The Black Album")
+ninety_nine_problems.save
+
